44491124503a2de4d67b0e3db736584d809b3364
catakevin98 <49349875+catakevin98@users.noreply.github.com>
2020-08-14 20:59:30 -0300

    Update loading game
    Realizado atualizações na parte de carregamento do game

--- server-data/resources/[vrp]/vrp_loading/__resource.lua
+++ server-data/resources/[vrp]/vrp_loading/__resource.lua
@@ -1,14 +1,22 @@
-files {
-    'index.html',
-    'index.js',
-	'parado.ogg',
-	'logo.png',
-	'Loading.ogg',
-	'doremi.ogg',
-    'css/style.css',
-    'css/background.jpg'
-}
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 loadscreen 'index.html'
 
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'index.html',
+    'stylesheet.css',
+    'config.js',
+    'app.js',
+    'config.css',
+    'imgs/bg0.png', 
+    'imgs/bg1.png', 
+    'imgs/bg2.png', 
+    'imgs/bg3.png', 
+    'imgs/bg4.png', 
+    'imgs/bg5.png', 
+    'imgs/bg6.png', 
+    'imgs/bg7.png',
+    'imgs/logo.png'
+}
---
+++ server-data/resources/[vrp]/vrp_loading/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 8000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "8JUoT5VuGxk",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 70,
+    "text": {
+        "title": "Servidor xesquelandia",
+        "link": "Bem-vindo a Xesquelandia!",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.png', 'bg2.png']
+}
